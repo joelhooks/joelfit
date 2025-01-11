081858985ca9bdb0d8242b666d69e23ba2906a0d
joel <joelhooks@gmail.com>
2025-01-11 09:44:45 -0800

    fix: add navigation menu exports
    Add NavigationMenu component exports to UI package

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,5 +2,6 @@ export * from './components/ui/button.js'
 export * from './components/ui/card.js'
 export * from './components/ui/scroll-area.js'
 export * from './components/ui/sheet.js'
+export * from './components/ui/navigation-menu.js'
 
 export { cn } from './lib/utils.js' 
