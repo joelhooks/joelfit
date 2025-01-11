937ca7079d1c1b79120f3292a9312a140879fc66
joel <joelhooks@gmail.com>
2025-01-11 13:11:33 -0800

    refactor: remove unnecessary hooks and providers from core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,1 @@
-export * from './components/async-data.js'
-export * from './components/repository-provider.js'
-export * from './hooks/use-repository.js'
-export * from './types.js' 
+export type { Entity, Repository } from './types.js' 
