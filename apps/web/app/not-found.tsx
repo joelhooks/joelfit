99595f8d882f714cff57437c844e3cc3dbc64c7f
joel <joelhooks@gmail.com>
2025-01-26 12:57:04 -0800

    fix: try again

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,10 @@
+import {NotFound} from '@repo/ui'
+
+export default function NotFoundPage() {
+  return (
+    <NotFound
+      title="Page Not Found"
+      message="Sorry, we couldn't find the page you're looking for."
+    />
+  )
+} 
